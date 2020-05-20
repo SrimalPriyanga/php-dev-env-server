a52ac4d042a373d5ccb7c53773458ca5347e0838
tsaduranga <tsaduranga@gmail.com>
2020-05-20 17:22:58 +0530

    login api update

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\School;
+
 //use App\Http\Controllers\API\BaseController as BaseController;
 
 class AuthController extends Controller
@@ -17,16 +19,29 @@ class AuthController extends Controller
         ]);
 
         if(!auth()->attempt($loginData)){
-            return response(['massage'=>'Invalid Credentials']);
+            return response(['massage'=>'Invalid Credentials'],401);
         }
 
-       // $success['token'] =  $loginData->createToken('authToken')->accessToken;
+        // $success['token'] =  $loginData->createToken('authToken')->accessToken;
 
-        $accessToken= auth()->user()->createToken('authToken')->accessToken;
-        return response(['user'=>auth()->user(),$accessToken]);
+        // $accessToken= auth()->user()->createToken('authToken')->accessToken;
+        //return response(['user'=>auth()->user(),$accessToken]);
+
+        $user = auth()->user();
+
+        $tokenResult = $user->createToken('authToken');
+        $token = $tokenResult->token;
+
+        return response()->json([
+            'user'=>auth()->user(),
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+        ]);
+
+
     }
 
     public function index(){
-
+        return response(['schools'=>School::all()],200);
     }
 }
